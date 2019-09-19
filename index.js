43cc733dcc61900ca117e787dd5fa63fb729a51c
searching-form-5732 <kevin.downs04@gmail.com>
2019-09-19 23:26:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 function iterativeLog(array){
+<<<<<<< HEAD
   array.forEach((element, index) => {
     console.log(`${index}: ${element}`)
   })
@@ -13,4 +14,7 @@ function iterate(callback){
 
 function doToArray(array, callback){
   array.forEach(callback)
+=======
+  array.forEach((element, index) => {console.log(`${element}: ${index}`)})
+>>>>>>> 7a8120d968bd455c1d3609ac251f36016a2658ea
 }
